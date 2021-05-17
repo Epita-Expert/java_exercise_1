af2440a29d9cda9bdb379ad350e1e1a5824e165a
Minaro <paturetromain@gmail.com>
2021-05-17 15:35:26 +0200

    Add  command

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Welcome");
+        try ( Scanner scanner = new Scanner( System.in ) ) {
+            do {
+                System.out.print( "Veuillez saisir une commande : " );
+            } while (!scanner.next().equals("quit"));
+        }
     }
 }
